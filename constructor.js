09799d0f601ba96c97807ceda34b2d96548bc6ce
roigr <roigrosa88@gmail.com>
2018-12-22 03:47:34 -0500

    separated js file added

---
+++ constructor.js
@@ -0,0 +1,8 @@
+class Pokemon {
+    constructor(hp, attack, defense, abilities) {
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.abilities = abilities;
+    }
+}
